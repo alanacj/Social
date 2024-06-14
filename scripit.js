3ee42b8df7a911a3e0953dc87fcd92df860aad0d
Alana Carolini <105944410+alanacj@users.noreply.github.com>
2024-06-14 11:55:34 -0300

    Create scripit.js

---
+++ scripit.js
@@ -0,0 +1,98 @@
+
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultados = document.querySelector(".caixa-Resultados");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "vc gosta de estuda?",
+        alternativas: [
+            {
+                texto: "sim",
+                afirmacao: "afirmacao"
+            },
+            {
+                texto: "não",
+                afirmacao: "afirmacao"
+            },
+        ]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            {
+                texto: "sim",
+                afirmacao: "afirmacao"
+            },
+            {
+                texto: "não",
+                afirmacao: "afirmacao"
+            },
+        ]
+    },
+    {
+        enunciado: "Pergunta 3",
+        alternativas: [
+            {
+                texto: "sim",
+                afirmacao: "afirmacao"
+            },
+            {
+                texto: "não",
+                afirmacao: "afirmacao"
+            },
+        ]
+    },
+    {
+        enunciado: "Pergunta 4",
+        alternativas: [
+            {
+                texto: "sim",
+                afirmacao: "afirmacao"
+            },
+            {
+                texto: "não",
+                afirmacao: "afirmacao"
+            },
+        ]
+    },
+    {
+        enunciado: "Pergunta 5",
+        alternativas: [
+            {
+                texto: "sim",
+                afirmacao: "afirmacao"
+            },
+            {
+                texto: "não",
+                afirmacao: "afirmacao"
+            },
+        ]
+    }
+];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+function respostaSelecionada(opcaoSelecionada){
+
+}
+mostraPergunta();
+
